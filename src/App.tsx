8aa9e05fd45c6aa784618060c9dab3f236fe34f3
Olexandr Ohurtsov <oleksandr.ohurtsov@gmail.com>
2024-03-26 19:18:28 +0200

    error404, navlink

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import { PageOne } from './components/pages/PageOne';
+import { PageTwo } from './components/pages/PageTwo';
+import { PageThree } from './components/pages/PageThree';
+import { Error404 } from './components/pages/Error404';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 
 function App() {
@@ -8,13 +13,26 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <div><NavLink to='/page1' >Page1</NavLink></div>
+                    <div><NavLink to='/page2' >Page2</NavLink></div>
+                    <div><NavLink to='/page3' >Page3</NavLink></div>
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                    <Routes>
+                        <Route path="/" element={ <Navigate to={"/page1"}/>}/>
+                        <Route path="/page1" element={ <PageOne />}/>
+                        <Route path="/page2" element={ <PageTwo />}/>
+                        <Route path="/page3" element={ <PageThree />}/>
+
+                        
+                        <Route path="/error404" element={ <Error404 />}/>
+                        <Route path="/*" element={ <Navigate to={"/error404"}/>}/>
+                        
+                        
+                    </Routes>
                 </div>
             </div>
-            <div className={styles.footer}>abibas 2023</div>
+            <div className={styles.footer}>abibas 2024</div>
         </div>
     );
 }
